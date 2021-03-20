53d0d270d84d7051b737c4d6bf38761ccdbf831c
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-21 02:31:14 +0800

    Default Changelist

--- framework/core/Dispatcher.php
+++ framework/core/Dispatcher.php
@@ -2,13 +2,13 @@
 /**
  * GitHub:https://github.com/SouthWind017/
  * Name:zhang tian yu
- * CreateTime:2021/3/21 2:16
+ * CreateTime:2021/3/21 0021 02:28
  * IdeName:PhpStorm
  * FileName:EasyPhp
  * @copyright (c) EasyPhp All Rights Reserved
  */
-define('EP_ROOT',dirname(__FILE__).'/');
-define('TIMESTAMP', time());
+namespace app\core;
 
-use app\core\Dispatcher;
-use app\core\Database;
+class Dispatcher{
+
+}
