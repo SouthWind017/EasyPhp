4ec19af1812a2ea96c4600c0660208fb5941b5e2
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-21 02:20:19 +0800

    Default Changelist

---
+++ framework/core.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * GitHub:https://github.com/SouthWind017/
+ * Name:zhang tian yu
+ * CreateTime:2021/3/21 2:16
+ * IdeName:PhpStorm
+ * FileName:EasyPhp
+ * @copyright (c) EasyPhp All Rights Reserved
+ */
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * GitHub:https://github.com/SouthWind017/
+ * Name:zhang tian yu
+ * CreateTime:2021/3/21 2:05
+ * IdeName:PhpStorm
+ * FileName:EasyPhp
+ * @copyright (c) EasyPhp All Rights Reserved
+ */
+//定义文件路径常量
+define('APP_FILE',dirname(__FILE__).'/');
+//引入核心文件
+require_once APP_FILE.'framework/core.php';
