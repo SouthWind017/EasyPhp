81d120f94d37555c36eb748305e046f08d5bdc6b
张天宇 <65751313+SouthWind017@users.noreply.github.com>
2021-03-31 23:32:00 +0800

    Delete app/index directory

--- app/index/controller/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * GitHub:https://github.com/SouthWind017/
- * Name:zhang tian yu
- * CreateTime:2021/3/21 0021 02:39
- * IdeName:PhpStorm
- * FileName:EasyPhp
- * @copyright (c) EasyPhp All Rights Reserved
- */
