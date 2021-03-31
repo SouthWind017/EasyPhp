9f63d2242f251e5a0b0f68a07ee6cb55046b367a
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-04-01 02:02:46 +0800

    数据库封装类

--- model/Db.php
+++ model/Db.php
@@ -17,7 +17,7 @@ class Db
         'user'=>'root',  //用户名
         'pass'=>'root',  //密码
         'charset'=>'utf8', //字符集
-        'dbname'=>'test' //字符集
+        'dbname'=>'test' //表名
     ];
 
     private static $instance = null;
@@ -26,6 +26,29 @@ class Db
 
     private function __construct(){
 
+        $this->connect();
+
+    }
+    private function __clone(){
+
+    }
+    public static function getInstance(){
+        if(!self::$instance instanceof self){
+            self::$instance=new self();
+        }
+        return self::$instance;
+
+    }
+    private function connect(){ //数据库连接
+        try {
+            $dsn = "{$this->dataBaseCfg['db']}:host={$this->dataBaseCfg['host']};port={$this->dataBaseCfg['port']};dbname={$this->dataBaseCfg['dbname']};charset={$this->dataBaseCfg['charset']}";
+            //创建PDO对象
+            $this->conn = new PDO($dsn,$this->dataBaseCfg['user'],$this->dataBaseCfg['pass']);
+            //设置字符集
+            $this->conn->query("SET NAMES {$this->dataBaseCfg['charset']}");
+        }catch (PDOException $e){
+            die('数据库连接失败'.$e->getMessage());
+        }
     }
 
 
