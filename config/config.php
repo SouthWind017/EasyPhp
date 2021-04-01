42b439236779eef66fcc6437129a3d1264a6445d
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-04-01 08:58:04 +0800

    系统配置文件

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * GitHub:https://github.com/SouthWind017/
+ * Name:zhang tian yu
+ * CreateTime:2021/4/1 0001 07:06
+ * IdeName:PhpStorm
+ * FileName:EasyPhp
+ * @copyright (c) EasyPhp All Rights Reserved
+ */
+
+return [
+    'db'=>[
+        'user'=>'easyphp',
+        'pass'=>'LFRpxFEzPF8hpGx5',
+        'dbname'=>'easyphp'
+    ],
+    'application'=>[
+        'default_module'          => 'index',
+        'default_controller'      => 'Index',
+        'default_action'          => 'index',
+        'default_view'            => 'index',
+        'default_view_suffix'     => 'html',
+    ]
+
+];
